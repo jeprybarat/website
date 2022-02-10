ce2406c51e71bed7ff4edfdcc7b38899f40f9602
jefri hutabarat <jeprybarat27@gmail.com>
2022-02-10 13:12:43 +0700

    gas

--- js/wafloatbox-0.2.js
+++ js/wafloatbox-0.2.js
@@ -5,7 +5,7 @@ jQuery.fn.extend({
         var a = $(this)
         a.prepend(`
         <div class="myk-btn">
-            <img src="images/wa/whatsapp.png" class="myk-wa-icon">
+            <img src="images/wa/whatsapp.svg" class="myk-wa-icon">
         </div>
         <div class="myk-panel"></div>
         `)
@@ -22,7 +22,7 @@ jQuery.fn.extend({
         $('.myk-item').each(function(){
         //this wrapped in jQuery will give us the current .letter-q div
         $('.myk-panelbody').append(`
-        <a target='_blank' href="https://wa.me/${$(this).data('wanumber')}?text=I'm%20inquiring%20about%20the%20apartment%20listing" class="myk-list">
+        <a target='_blank' href="https://wa.me/${$(this).data('wanumber')}?text=hai Saya mau konsultasi" class="myk-list">
             <img src="${$(this).data('waava')}" class="myk-ava">
             <div class="myk-number">
                 <p class="myk-who"><b>${$(this).data('wadivision')}</b></p>
